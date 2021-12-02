25c03733717ba39d19010e8dd2bf08a5dbef8941
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-02 16:46:04 +0100

    bonus 3 js error

--- bonus-3/js/script-bonus-3.js
+++ bonus-3/js/script-bonus-3.js
@@ -1,9 +1,15 @@
 // calcolatrice per tutti i calcoli 
 
+// prompt operatore
+const operatorX = prompt('Inserisci quale operazione vuoi eseguire "+ o - o * o /" ');
+
 // prompt primo numero 
-
+const firstN = prompt('Inserisci il primo numero da calcolare');
 // prompt secondo numero
-
-// prompt terzo numero 
+const secondN = prompt('Inserisci il secondo numero da calcolare');
 
 // risultato visibile nella calcolatrice
+let resultN = `${firstN} ${operatorX} ${secondN}`;
+
+let htmlElement = document.getElementById('risultatoN');
+htmlElement.innerHTML = `${resultN}`;
