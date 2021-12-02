3e45c715c5280b98c8950a4d2de6df3080807bb8
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-02 15:15:22 +0100

    second bonus prompt calculator v.1

---
+++ bonus-2/js/script-bonus-2.js
@@ -0,0 +1,6 @@
+const firstN = prompt('inserisci primo numero');
+const secondN = prompt('inserisci secondo numero');
+
+let resultN = firstN * secondN;
+
+console.log(`${firstN} * ${secondN} = ${resultN}`);
