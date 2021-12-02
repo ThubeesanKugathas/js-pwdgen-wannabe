83948be6a555f20445db46ec2cdba3511dc3649e
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-02 14:24:15 +0100

    js's prompt, style.css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 // 3 prompt 
 // Chiedere all'utente il nome (prompt 1)
+const userFirstname = prompt('Inserisci il tuo Nome');
 
 // Chidere all'utente il cognome (prompt 2)
+const userLastname = prompt('Inserisci il tuo Cognome');
 
 // Chiedere all'utente il colore preferito (prompt 3) 
+const userFavcolor = prompt('Inserisci il tuo Colore Preferito');
+
+// Immettere variabile sul div "pdwgen" con i prompt  
+let htmlElement = document.getElementById('password');
+htmlElement.innerHTML = `La tua Password Personalizzata è: ${userFirstname}${userLastname}${userFavcolor}21`;
+
+
